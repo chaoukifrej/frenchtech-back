7d8c07412d69b1b67094158346cbe666a4e16cb9
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-05-11 21:58:48 +0200

    creation tables

---
+++ database/seeds/BufferSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BufferSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
