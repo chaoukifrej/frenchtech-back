35b108010bb9939c473e639e98105b257897e553
alexandre Dalmar <alexandredalmar@gmail.com>
2021-05-20 14:07:01 +0200

    midleware route admin get actors non fonctionelle

--- routes/api.php
+++ routes/api.php
@@ -64,7 +64,7 @@ Route::prefix('admin')->group(function () {
         Route::get('logout', 'Auth\LoginController@logoutAdmin')->name('login.logoutAdmin');
 
         //GET ALL ACTORS WITH ALL INFOS
-        Route::get('actors', 'ActorController@getAllInfosActors')->name('actor.getAllInfosActors')->middleware('auth:admin');
+        Route::get('actors', 'ActorController@getAllInfosActors')->name('actor.getAllInfosActors');
     });
 
     //?ROUTES EN POST
