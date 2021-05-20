dc3ee64a1615238130eb6c7fecb0e3317f3dc194
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-05-20 12:11:07 +0200

    creation d'une route getall infos actors tab + travail sur excel

--- app/Http/Middleware/CheckifAdmin.php
+++ app/Http/Middleware/CheckifAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CheckifAdmin
 {
@@ -15,6 +16,7 @@ class CheckifAdmin
      */
     public function handle($request, Closure $next)
     {
+
         if (Auth::guard('admin')->check()) {
             return $next($request);
         }
