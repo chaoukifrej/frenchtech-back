5d6a9dc221f37a4482938523a1a318be4ca18254
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-05-25 21:03:12 +0200

    change in validator BufferController

--- app/Http/Controllers/BufferController.php
+++ app/Http/Controllers/BufferController.php
@@ -83,35 +83,32 @@ class BufferController extends Controller
         try {
             $buffer = Buffer::find($request->id);
 
-            Validator::make(
-                $request->all(),
-                [
-                    'logo' => ['string'],
-                    'name' => ['string', 'max:64'],
-                    'adress' => ['string', 'max:64'],
-                    'postal_code' => ['integer', 'max:5'],
-                    'city' => ['string', 'max:64'],
+            $request->validate([
+                'logo' => ['string'],
+                'name' => ['string', 'max:64'],
+                'adress' => ['string', 'max:64'],
+                'postal_code' => ['integer', 'max:5'],
+                'city' => ['string', 'max:64'],
 
-                    'email' => ['string', 'email', 'max:64', 'unique:actors'],
-                    'phone' => ['string', 'max:20'],
-                    'category' => ['string', 'max:64'],
-                    'associations' => ['nullable', 'string', 'max:64'],
-                    'description' => ['string'],
+                'email' => ['string', 'email', 'max:64', 'unique:actors'],
+                'phone' => ['string', 'max:20'],
+                'category' => ['string', 'max:64'],
+                'associations' => ['nullable', 'string', 'max:64'],
+                'description' => ['string'],
 
-                    'facebook' => ['nullable', 'string'],
-                    'twitter' => ['nullable', 'string'],
-                    'linkedin' => ['nullable', 'string'],
-                    'website' => ['nullable', 'string'],
+                'facebook' => ['nullable', 'string'],
+                'twitter' => ['nullable', 'string'],
+                'linkedin' => ['nullable', 'string'],
+                'website' => ['nullable', 'string'],
 
-                    'activity_area' => ['string', 'max:64'],
-                    'funds' => ['numeric'],
-                    'employees_number' => ['integer'],
-                    'jobs_available_number' => ['integer'],
-                    'women_number' => ['integer'],
-                    'revenues' => ['numeric'],
+                'activity_area' => ['string', 'max:64'],
+                'funds' => ['numeric'],
+                'employees_number' => ['integer'],
+                'jobs_available_number' => ['integer'],
+                'women_number' => ['integer'],
+                'revenues' => ['numeric'],
+            ]);
 
-                ],
-            )->validate();
 
             if (!isset($request->name)) {
                 $buffer->name = $buffer->name;
